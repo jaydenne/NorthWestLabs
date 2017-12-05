f1108149c2b5bb74d12836f1c4af2831d03bc16d
Jesse Nelson <J.nelson55@gmail.com>
2017-12-05 15:46:48 -0700

    test

---
+++ NorthWestLabs/Controllers/SingaporeController.cs
@@ -0,0 +1,22 @@
+﻿using NorthWestLabs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Controllers
+{
+    public class SingaporeController : Controller
+    {
+        private NorthwestLabsEntitiesDB db = new NorthwestLabsEntitiesDB();
+
+        // GET: Singapore
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
