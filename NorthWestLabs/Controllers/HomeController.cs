f0bebdd79154cf7328c7172d23a32c67c2b9e12c
Jesse Nelson <J.nelson55@gmail.com>
2017-12-05 11:39:06 -0700

    models added

--- NorthWestLabs/Controllers/HomeController.cs
+++ NorthWestLabs/Controllers/HomeController.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace NorthWestLabs.Controllers
 {
     public class HomeController : Controller
     {
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection form, string returnUrl, bool rememberMe = false)
+        {
+            String user = form["Username"].ToString();
+            String password = form["Password"].ToString();
+
+            if(string.Equals(user, "Missouri") && (string.Equals(password, "ShowMe")))
+            {
+                FormsAuthentication.SetAuthCookie(user, rememberMe);
+
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                    
+                return RedirectToAction("UpdateData");
+
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
         public ActionResult Index()
         {
             return View();
