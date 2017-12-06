e6c3fb01cb120cb3b00582457d77bd3cff6eb167
Jesse Nelson <J.nelson55@gmail.com>
2017-12-05 19:05:22 -0700

    add classes

---
+++ NorthWestLabs/Models/AssayOrder.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthWestLabs.Models
+{
+
+    [Table("Client")]
+    public class AssayOrder
+    {
+        public int AssayOrderID { get; set; }
+        public int WorkOrderID { get; set; }
+        public int PriorityLevelID { get; set; }
+        public int AssayID { get; set; }
+        public string ModifiedBy { get; set; }
+        public System.DateTime ModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+    
+       /* public virtual PriorityLevel PriorityLevel { get; set; }
+        public virtual ProtocolNotebook ProtocolNotebook { get; set; }
+        public virtual WorkOrder WorkOrder { get; set; }
+        public virtual ICollection<AssayOrderComment> AssayOrderComments { get; set; }
+        public virtual ICollection<AssayOrderReport> AssayOrderReports { get; set; }
+        public virtual ICollection<TestResult> TestResults { get; set; }
+ */
+    }
+}
