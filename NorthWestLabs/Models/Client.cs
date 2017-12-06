11885eaa92b013727a1d4c7fc18260810c308cd8
Jesse Nelson <J.nelson55@gmail.com>
2017-12-05 18:42:57 -0700

    IT WORKS!

--- NorthWestLabs/Models/Client.cs
+++ NorthWestLabs/Models/Client.cs
@@ -21,7 +21,7 @@ namespace NorthWestLabs.Models
         public string City { get; set; }
         public string State { get; set; }
         public string Country { get; set; }
-        public int? Zip { get; set; }
+        public Nullable<int> Zip { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
