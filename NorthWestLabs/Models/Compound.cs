2585fc0e9cc819685354f02a6d93cee4173b23db
avery <avery@DESKTOP-25UFBQ3>
2017-12-07 22:06:43 -0700

    More changes

--- NorthWestLabs/Models/Compound.cs
+++ NorthWestLabs/Models/Compound.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -15,17 +16,26 @@ namespace NorthWestLabs.Models
         [Key]
         public int CompoundID { get; set; }
         public int LTNumber { get; set; }
+        [DisplayName("Sequence Code")]
         public int SequenceCode { get; set; }
+        [DisplayName ("Compound Name")]
         public string CompoundName { get; set; }
         public Nullable<int> Quantity { get; set; }
+        [DisplayName("Date Arrived")]
         public Nullable<System.DateTime> DateArrived { get; set; }
+        [DisplayName("Received By")]
         public string ReceivedBy { get; set; }
+        [DisplayName("Date Due")]
         public Nullable<System.DateTime> DateDue { get; set; }
         public string Appearance { get; set; }
         public Nullable<decimal> Weight { get; set; }
+        [DisplayName("Molecular Mass")]
         public Nullable<decimal> MolecularMass { get; set; }
+        [DisplayName("Confirmation Date")]
         public Nullable<System.DateTime> ConfirmationDate { get; set; }
+        [DisplayName("Max Total Dose")]
         public Nullable<decimal> MaxTotalDose { get; set; }
+        [DisplayName("Actual Weight")]
         public Nullable<decimal> ActualWeight { get; set; }
         public string ModifiedBy { get; set; }
         public System.DateTime ModifiedDate { get; set; }
